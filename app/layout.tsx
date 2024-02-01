b081b8c799a10a4b9e8445707fd4bbc57af991e5
Maximilian <mpinderwhite@gmail.com>
2024-02-01 17:10:27 +0100

    [+] supabase provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
 export const metadata: Metadata = {
   title: "Spotify Clone",
@@ -13,7 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SupabaseProvider>
+          {children}
+        </SupabaseProvider>
+      </body>
     </html>
   );
 }
